10301f5b366e8e511cf6907b64a5a40ad5fe2d2e
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-06-18 23:31:13 -0400

    Corrección de errores

--- src/main/java/com/ranking/microservicio_ranking/config/SwaggerConfig.java
+++ src/main/java/com/ranking/microservicio_ranking/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.ranking.microservicio_ranking.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,24 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI(){
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("API Microservicio Ranking")
                         .version("1.0")
-                        .description("Documentación de la API para el microservicio Ranking"));
+                        .description("Documentación de la API para el microservicio Ranking")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .description("TOKEN JWT")
+                        )
+                );
+
     }
 }
